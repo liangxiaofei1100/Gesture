17ce2d0ff2784708f26f9a96231a19a044970ebb
Albert <stevenliu1988@163.com>
2014-09-05 16:14:03 +0800

    slide setting view

--- src/com/zhaoyan/gesture/more/SettingsActivity.java
+++ src/com/zhaoyan/gesture/more/SettingsActivity.java
@@ -1,8 +1,9 @@
 package com.zhaoyan.gesture.more;
 
-
+import com.zhaoyan.gesture.R;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 public class SettingsActivity extends BaseFragmentActivity {
@@ -13,9 +14,10 @@ public class SettingsActivity extends BaseFragmentActivity {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		new DirectLogin(this).login();
+		setContentView(R.layout.account_setting_b);
 		mWoFragment = new WoFragment();
 		getFragmentManager().beginTransaction()
-				.replace(android.R.id.content, mWoFragment).commit();
+				.replace(R.id.root_layout, mWoFragment).commit();
 	}
 
 }
